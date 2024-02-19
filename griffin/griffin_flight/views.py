faab9cabe7f31c3fb0ecfea158bd87d315118fd1
slee3593 <slee3593@gatech.edu>
2024-02-19 17:21:08 +0900

    add: serializer, db연결 feat: model db에 맞춰 수정

--- griffin/griffin_flight/views.py
+++ griffin/griffin_flight/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import  Admins, Books, Airplanes, Airports, Flights, Passports, Users
+from .serializers import AdminsSerializer, BooksSerializer
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
+
+def admin(request):
+    if request.method == 'GET':
+        snippets = Admins.objects.all()
+        serializer = AdminsSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = AdminsSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
